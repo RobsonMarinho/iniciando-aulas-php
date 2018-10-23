daf00e1f0537746d1fcd2db544ad62095157148f
Robson Marinho <robson_luz15@hotmail.com>
2018-10-22 23:53:55 -0300

    DAO delete

---
+++ 11-banco-de-dados-data-acess-object-e-pdo/53-pdo-dao-delete/index.php
@@ -0,0 +1,43 @@
+<?php
+
+	require_once("config.php");
+
+//	Carrega um usuário
+//	$root = new Usuario();
+//	$root->loadbyId(3);
+//	echo $root;
+
+//	Carrega uma lista de usuários
+//	$lista = Usuario::getList();
+//	echo json_encode($lista);
+
+//	Carrega uma lista de usuários buscando pelo login
+//	$search = Usuario::search("jo");
+//	echo json_encode($search);
+
+//	Carrega o usuário usando o login e a senha
+//	$usuario = new Usuario();
+//	$usuario->login("root", "!@#$");
+
+//	echo $usuario;
+
+//Criando um novo usuario
+//$aluno = new Usuario("aluno", "@lun0");
+//$aluno->insert();
+//echo $aluno;
+
+//ALTERAR UM USUÁRIO
+//$usuario = new Usuario();
+//$usuario->loadById(8);
+//$usuario->update("professor", "!@#$%¨&*");
+//echo $usuario;
+
+$usuario = new Usuario();
+
+$usuario->loadById(7);
+
+$usuario->delete();
+
+echo $usuario;
+
+?>
