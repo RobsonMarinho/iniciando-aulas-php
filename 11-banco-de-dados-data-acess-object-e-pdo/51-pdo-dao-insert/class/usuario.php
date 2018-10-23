6ae1282db01e38546f0497c3da313d3f228a6155
Robson Marinho <robson_luz15@hotmail.com>
2018-10-22 23:24:31 -0300

    DAO insert

--- 11-banco-de-dados-data-acess-object-e-pdo/51-pdo-dao-insert/class/usuario.php
+++ 11-banco-de-dados-data-acess-object-e-pdo/51-pdo-dao-insert/class/usuario.php
@@ -50,14 +50,9 @@ class Usuario{
 
 		if (count($results) > 0) {
 
-			$row = $results[0];
+			$this->setData($results[0]);
 
-			$this->setIdusuario($row['idusuario']);
-			$this->setDeslogin($row['deslogin']);
-			$this->setDessenha($row['dessenha']);
-			$this->setDtcadastro(new DateTime($row['dtcadastro']));
-
-		}
+		}	
 	}
 
 //Retorna uma lista de usuários
@@ -73,7 +68,7 @@ class Usuario{
 		$sql = new Sql();
 
 		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(   ':SEARCH'=>"%".$login."%"
-		));
+	));
 
 	}
 
@@ -87,17 +82,40 @@ class Usuario{
 
 		if (count($results) > 0) {
 
-			$row = $results[0];
-
-			$this->setIdusuario($row['idusuario']);
-			$this->setDeslogin($row['deslogin']);
-			$this->setDessenha($row['dessenha']);
-			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+			$this->setData($results[0]);
 
 		} else {
 
 			throw new Exception("Login e/ou senha inválidos.");
 		}
+	}
+
+	public function setData($data){	
+
+		$this->setIdusuario($data['idusuario']);
+		$this->setDeslogin($data['deslogin']);
+		$this->setDessenha($data['dessenha']);
+		$this->setDtcadastro(new DateTime($data['dtcadastro']));
+	}
+
+	public function insert(){
+
+		$sql = new Sql();
+
+		$results = $sql->select("CALL sp_usuarios_insert(:LOGIN, :PASSWORD)", array(
+			':LOGIN'=>$this->getDeslogin(),
+			':PASSWORD'=>$this->getDessenha()
+		));
+
+		if (count($results) > 0) {
+			$this->setData($results[0]);
+		}
+	}
+
+	public function __construct($login= "", $password = ""){
+		
+		$this->setDeslogin($login);
+		$this->setDessenha($password);
 	}
 
 	public function __toString(){
--- 11-banco-de-dados-data-acess-object-e-pdo/51-pdo-dao-insert/index.php
+++ 11-banco-de-dados-data-acess-object-e-pdo/51-pdo-dao-insert/index.php
@@ -1,24 +1,30 @@
 <?php
 
-require_once("config.php");
-$usuario = new Usuario();
-$usuario->login("root", "!@#$");
-//Exibe a variável usuário
-echo $usuario;
+	require_once("config.php");
 
 //	Carrega um usuário
 //	$root = new Usuario();
 //	$root->loadbyId(3);
 //	echo $root;
 
-//Carrega uma lista de usuários
+//	Carrega uma lista de usuários
 //	$lista = Usuario::getList();
-//
 //	echo json_encode($lista);
 
-//Carrega uma lista de usuários buscando pelo login
-//$search = Usuario::search("jo");
-//echo json_encode($search);
+//	Carrega uma lista de usuários buscando pelo login
+//	$search = Usuario::search("jo");
+//	echo json_encode($search);
 
-//Carrega o usuário usando o login e a senha
+//	Carrega o usuário usando o login e a senha
+//	$usuario = new Usuario();
+//	$usuario->login("root", "!@#$");
+
+//	echo $usuario;
+
+$aluno = new Usuario("aluno", "@lun0");
+
+$aluno->insert();
+
+echo $aluno;
+
 ?>
