11d17a6619e67386887bb9e1cca443b3b0e19617
Robson Marinho <robson_luz15@hotmail.com>
2018-11-01 23:51:16 -0300

    Usando a cURL para busca de CEP

---
+++ 12-manipulando-arquivos/61-usando-a-curl/exemplo01.php
@@ -0,0 +1,18 @@
+<?php
+
+$cep = "50750040";
+$link = "https://viacep.com.br/ws/$cep/json/";
+
+$ch = curl_init($link);
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);	#Não valida o SSL
+
+$response = curl_exec($ch);
+
+curl_close($ch);
+
+$data = json_decode($response, true);
+
+print_r($data);
+?>
